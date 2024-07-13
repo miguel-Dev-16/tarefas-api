e6db19462775508adbc637d6746d6e141ae4f4d3
miguel-Dev-16 <miguelanalista7@gmail.com>
2024-07-13 09:56:10 -0300

    criação do projeto

---
+++ tarefas-api/src/test/java/com/treina_recife/tarefas_api/TarefasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treina_recife.tarefas_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarefasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
